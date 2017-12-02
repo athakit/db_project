bfe12b2e9435b00ded7cc6882afd09a1500acec6
Suicidy <phichayut.nine@gmail.com>
2017-12-02 22:51:16 +0700

    init commit

---
+++ app/Http/Controllers/PageController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class PageController extends Controller{
+    public function getIndex(){
+
+    }
+    public function getAbout(){
+        return "Hello this is About";
+    }
+    public function getContact(){
+
+    }
+}
